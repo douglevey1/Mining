1c8b3d47151ea4e870e844110415a94517c9db48
douglevey1 <douglevey1@gmail.com>
2019-09-15 20:47:45 -0400

    Part 1

---
+++ Assets/GameController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    float s;
+    Boolean ok = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        s = Time.time;
+        if (s > 3 && ok == false)
+        {
+            print("Three seconds have passed...");
+            ok = true;
+        }
+    }
+}
