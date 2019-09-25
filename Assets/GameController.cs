58432caa948b5d1ada4bd0f0ce355c30a640aad3
douglevey1 <douglevey1@gmail.com>
2019-09-25 11:16:08 -0400

    Part 3

--- Assets/GameController.cs
+++ Assets/GameController.cs
@@ -2,14 +2,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class GameController : MonoBehaviour
 {
-    int miningSpeed = 3;
-    int bronzeSupply = 3;
-    int silverSupply = 2;
-    int bronzeMined = 0;
-    int silverMined = 0;
+    public int miningSpeed = 3;
+    public int bronzeSupply = 3;
+    public int silverSupply = 2;
+    public int bronzeMined = 0;
+    public int silverMined = 0;
+    public int xPos = 0;
+    public GameObject cubeRed;
+    public GameObject cubeWhite;
+    public Vector3 position = new Vector3 (0, 0, 0);
     // Start is called before the first frame update
     void Start()
     {
@@ -20,15 +23,21 @@ public class GameController : MonoBehaviour
     {
         if (Time.time > miningSpeed)
         {
+            position = new Vector3(xPos, 0, 0);
             if (bronzeSupply == 0 && silverSupply > 0)
             {
                 silverMined++;
                 silverSupply--;
+                Instantiate(cubeWhite, position, Quaternion.identity);
+                xPos += 2;
             }
             if (bronzeSupply > 0)
             {
                 bronzeMined++;
                 bronzeSupply--;
+                
+                Instantiate(cubeRed, position, Quaternion.identity);
+                xPos += 2;
             }
             print("You have mined " + bronzeMined + " bronze and " + silverMined + " silver.");
             miningSpeed += 3;
